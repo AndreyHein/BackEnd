4f1a0b2422d70bb31f43eeb9a1c61f078e1fd7b9
AndreyHein <soly.a@web.de>
2024-08-13 20:19:58 +0200

    lesson_08

---
+++ lesson_08/code/user_service/src/main/java/de/ait/user_service/controller/UserController.java
@@ -0,0 +1,40 @@
+package de.ait.user_service.controller;
+
+import de.ait.user_service.entity.User;
+import de.ait.user_service.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserController {
+    private final UserService service;
+
+    // "api/users"  GET
+    // "api/users?name=jack
+    @GetMapping("/users")
+    public List<User> getAllUsers(@RequestParam(name = "name", required = false, defaultValue = "") String name) {
+        if (name.equals("")) {
+            return service.getAll();
+        } else {
+            return service.getByName(name);
+        }
+
+    }
+
+    // "/api/users/2"
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable Long id) {
+
+        return service.getUserById(id);
+    }
+
+    @PostMapping("/users")
+    public User addUser(@RequestBody User user) {
+        service.save(user);
+        return user;
+    }
+}
---
+++ lesson_08/code/user_service/src/main/java/de/ait/user_service/repository/UserRepositoryImpl.java
@@ -0,0 +1,39 @@
+package de.ait.user_service.repository;
+
+import de.ait.user_service.entity.User;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalLong;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+    private List<User> list = new ArrayList<>(List.of(
+            new User(1L, "Jack", "jack@gmail.com", "qwer1"),
+            new User(2L, "John", "john@gmail.com", "qwer2"),
+            new User(3L, "Anna", "anna@gmail.com", "qwer3"),
+            new User(5L, "Jack", "jack@gmail.com", "qwer5"),
+            new User(4L, "Lena", "lena@gmail.com", "qwer4")));
+
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(list);
+    }
+
+    @Override
+    public User save(User entity) {
+        long max = list.stream().mapToLong(u -> u.getId().longValue())
+                .max().getAsLong();
+        entity.setId(max + 1);
+        list.add(entity);
+        return entity;
+    }
+
+    @Override
+    public User delete(Long id) {
+        return null;
+    }
+}
